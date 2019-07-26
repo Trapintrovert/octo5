f304b193763f6819896fe3c0d8b03edb5a1405f1
trapintrovert <mcneo062@gmail.com>
2019-07-26 07:50:52 +0100

    regex

--- js/validation.js
+++ js/validation.js
@@ -1,8 +1,10 @@
 
 const email = document.getElementById('email');
+const message = document.getElementById('message');
+
 
 email.addEventListener('blur', valEmail);
-
+message.addEventListener('blur', valMessage);
 
 function valEmail() {
   const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
@@ -12,3 +14,13 @@ function valEmail() {
     email.classList.remove('is-invalid');
   }
 }
+
+function valMessage() {
+  const re = /[a-zA-Z0-9_-]/;
+  if(!re.test(message.value)) {
+    message.classList.add('is-invalid');
+  } else {
+    message.classList.remove('is-invalid');
+  }
+}
+// /^[a-z0-9_-]{3,16}$/
